a71a46590360486a2aaa1da56f25b68e0b78479c
Shivam Rajput <58025059+shiviraj@users.noreply.github.com>
2019-11-22 18:52:03 +0530

    update the records

--- src/updateRecords.js
+++ src/updateRecords.js
@@ -5,9 +5,24 @@ const readRecords = function() {
   return JSON.parse(records);
 };
 
+const updateRecords = function(details) {
+  let records = readRecords();
+  let record = {
+    beverage: details.beverage,
+    qty: details.qty,
+    date: details.date
+  };
+  if (records[details.empID] == undefined) {
+    records[details.empID] = [];
+  }
+  records[details.empID].push(record);
+  let updatedRecord = JSON.stringify(records);
+  fs.writeFileSync('./juiceRecord/juiceRecords.json', updatedRecord, 'utf8');
+};
+
 const updateTransaction = function(details) {
   let oldRecords = readRecords();
-  //updateRecords(details);
+  updateRecords(details);
   let message =
     'Transaction Recorded:\n' +
     'Employee ID,Beverage,Quantity,Date\n' +
