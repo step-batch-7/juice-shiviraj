d41ce373597b7641612ade0a1254eb385019228f
Shivam Rajput <58025059+shiviraj@users.noreply.github.com>
2019-11-28 10:08:58 +0530

    refactor code
    reomve unnecesasry lines

--- beverage.js
+++ beverage.js
@@ -2,8 +2,7 @@ const getTransactionDetails = require('./src/beverageLib.js')
   .getTransactionDetails;
 const processTransaction = require('./src/beverageLib').processTransaction;
 const recordFile = './juiceRecord/juiceRecords.json';
-const fsF = require('fs');
-const fs = { readFileSync: fsF.readFileSync, writeFileSync: fsF.writeFileSync };
+const fs = require('fs');
 
 const main = function() {
   console.log('Anna Juice Ltd');
